8b0a90d24551c293b075347214228e2c55e77f0b
Matthias Tafelmeier <matthias.tafelmeier@gmx.net>
2017-05-10 11:54:18 +0200

    irq_reeler: semantic refactor
    Signed-off-by: Matthias Tafelmeier <matthias.tafelmeier@gmx.net>

--- irq/irq_reeler.py
+++ irq/irq_reeler.py
@@ -26,6 +26,7 @@ from tabulate import tabulate
 PROC_FS_S_IRQS = "/proc/softirqs"
 PROC_FS_H_IRQS = "/proc/interrupts"
 
+
 class Irq:
     def __init__(self, name, data, _range):
         if _range:
@@ -40,7 +41,7 @@ class Irq:
     def cpu_share_percent(self):
         total = self.total()
         if total != 0:
-            return [ float( (irq_n * 100 ) / total ) for irq_n  in self.occurrences ]
+            return [float((irq_n * 100) / total) for irq_n in self.occurrences]
         else:
             return self.occurrences[:]
 
@@ -50,6 +51,7 @@ class Irq:
     def __getitem__(self, i):
         return self.occurrences[i]
 
+
 def procfs_read_raw(_file):
     raw = ""
     with open(_file, "r") as irqs_f:
@@ -57,88 +59,101 @@ def procfs_read_raw(_file):
 
     return raw
 
+
 def form_irq_abstraction(raw, args):
     _Irqs = []
     raw_list = raw.splitlines()
     col_head = raw_list[0].split()
-    for raw_l in raw_list[1:]: 
+    for raw_l in raw_list[1:]:
         raw_l_refined = raw_l.split()
         name = raw_l_refined[0]
         if args.hard:
-            if len (raw_l_refined[1:len(col_head)+1]) < len(col_head):
+            if len(raw_l_refined[1:len(col_head)+1]) < len(col_head):
                     continue
             name = "%s %s" % ("".join(raw_l_refined[len(col_head)+1:]), name)
-        _Irqs.append(Irq(name, [ int(i) for i in raw_l_refined[1:len(col_head)+1] ], args._range))
+        _Irqs.append(Irq(name,
+                     [int(i) for i in raw_l_refined[1:len(col_head)+1]],
+                     args._range))
     return col_head, _Irqs
+
 
 def merge_rate_meas(abstr_l, delta):
     merged_Irq_abstr = []
     diff = lambda x: float((x[0] - x[1]) / delta)
     for elem1, elem2 in map(None, abstr_l[0], abstr_l[1]):
         elem2.occurrences = map(diff, zip(elem2.get_occ(), elem1.get_occ()))
-        merged_Irq_abstr.append( elem2 )
+        merged_Irq_abstr.append(elem2)
 
-    return merged_Irq_abstr 
+    return merged_Irq_abstr
+
+
+def determ_focus(args):
+    return PROC_FS_H_IRQS if args.hard else PROC_FS_S_IRQS
+
+
+def do_outp(col_head, _Irqs, args):
+    table = []
+
+    if args.sort:
+        index = int(args.sort)
+        key_f = lambda x: x[index]
+        cmp_f = lambda x, y: y - x
+        _Irqs = sorted(_Irqs, cmp=cmp_f, key=key_f)
+
+    if args._range:
+        col_head = col_head[args._range[0]:args._range[1]]
+
+    if args.total:
+        col_head.append("total")
+
+    for irq in _Irqs:
+        if args.perc:
+            outl = irq.cpu_share_percent()
+        else:
+            outl = irq.get_occ()
+        if args._range:
+            outl = outl[args._range[0]:args._range[1]]
+        if args.total:
+            outl.append(irq.total())
+        outl.insert(0, irq.name)
+        table.append(outl)
+
+    print tabulate(table, headers=col_head, tablefmt="plain")
 
 
 def run(args):
     raw = []
     Irqs = []
     col_head = []
+    focus = determ_focus(args)
 
     if args._range:
-        args._range = [ int(x) for x in args._range.strip().split("-")]
+        args._range = [int(x) for x in args._range.strip().split("-")]
         args._range[1] = args._range[1] + 1
 
-    if not args.rate:
-        raw = procfs_read_raw(PROC_FS_H_IRQS if args.hard else PROC_FS_S_IRQS)
-        col_head, _Irqs = form_irq_abstraction(raw, args)
-    else:
+    if args.rate:
         delta = int(args.rate)
         raw_l = []
-        raw_l.append(procfs_read_raw(PROC_FS_H_IRQS if args.hard else PROC_FS_S_IRQS))
-        time.sleep( delta )
-        raw_l.append(procfs_read_raw(PROC_FS_H_IRQS if args.hard else PROC_FS_S_IRQS))
+        raw_l.append(procfs_read_raw(focus))
+        time.sleep(delta)
+        raw_l.append(procfs_read_raw(focus))
         abstr_l = []
         for raw in raw_l:
             col_head, _Irq_abstr = form_irq_abstraction(raw, args)
             abstr_l.append(_Irq_abstr)
         _Irqs = merge_rate_meas(abstr_l, delta)
+    else:
+        raw = procfs_read_raw(focus)
+        col_head, _Irqs = form_irq_abstraction(raw, args)
 
-    if args._range:
-        col_head = col_head[args._range[0]:args._range[1]]
+    do_outp(col_head, _Irqs, args)
 
-    if args.total:
-        col_head.append("total") 
-
-    if args.sort:
-        index = int(args.sort)
-        key_f = lambda x: x[index]
-        cmp_f = lambda x,y: y - x
-        _Irqs = sorted(_Irqs, cmp=cmp_f, key=key_f)
-
-    table = []
-    for irq in _Irqs:
-        if args.perc:
-            outl = irq.cpu_share_percent()
-        else:
-            outl = irq.get_occ()
-        if args.total:
-            outl.append(irq.total())
-        if args._range:
-            outl = outl[args._range[0]:args._range[1]]
-        outl.insert(0, irq.name)
-        table.append(outl)
-
-    print tabulate(table, headers=col_head, tablefmt="plain")
- 
 
 if __name__ == "__main__":
-    
     parser = argparse.ArgumentParser(description='Frontend to irqs (soft | hard) proc fs')
     parser.add_argument("-p", '--perc', default=False, help='show CPU taken by IRQ in percentage', action='store_true')
     parser.add_argument("-t", dest="total", default=False, help='total up irqs per channel', action='store_true')
-    parser.add_argument("-s", dest="sort", help='CPU num to sort by') 
+    parser.add_argument("-s", dest="sort", help='CPU num to sort by')
     parser.add_argument("-r", "--rate", dest="rate", help='determine rate per sec for interval', metavar="SECS")
     parser.add_argument("--hard", dest="hard", help='showing hardirqs', default=False, action='store_true')
     parser.add_argument("--range", dest="_range", help='cpu range to focus', default=None, metavar="CPUX-CPUY")
